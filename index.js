7bc24baed237d0f5355ca3c93524e7148153bca4
Feuscel <feuscel@gmail.com>
2022-06-01 11:22:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+
+}
+
+function create ()
+{
+
+}
