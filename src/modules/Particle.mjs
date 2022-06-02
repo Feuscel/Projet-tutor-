964947adbcd388c06176ee53ce9ec86a77c28f77
Feuscel <feuscel@gmail.com>
2022-06-02 16:27:36 +0200

    Add intensity light

--- src/modules/Particle.mjs
+++ src/modules/Particle.mjs
@@ -1,9 +1,10 @@
 import { Ray } from "./Ray.mjs";
 export class Particle{
-    constructor(pos){
+    constructor(pos,intensity = 200){
         this.pos = new Phaser.Math.Vector2(window.innerWidth / 2, window.innerHeight / 2);
         this.rays = [];
-        for (var i = 0; i < 360; i += 1){
+        this.intensity = intensity;
+        for (var i = 0; i < 360; i += 0.1){
             var ray = new Ray(this.pos, i);
             this.rays.push(ray); 
         }
@@ -15,15 +16,17 @@ export class Particle{
     }
 
     look(walls, graphics) {
+
         for (let ray of this.rays){
             let closest = null;
             let record = Infinity;
+            let d;
             for (let wall of walls) {
                 
                 var point = ray.cast(wall);
-
+                
                 if(point){
-                    const d = this.pos.distance(point);
+                    d = this.pos.distance(point);
 
                     if(d < record){
                         record = d;
@@ -34,7 +37,11 @@ export class Particle{
                 }
             }
             if(closest){
-                const line = new Phaser.Geom.Line(this.pos.x, this.pos.y, closest.x, closest.y);
+                var line = new Phaser.Geom.Line(this.pos.x, this.pos.y, closest.x, closest.y);
+                if(this.pos.distance(closest) > this.intensity){
+                    var line = Phaser.Geom.Line.Extend(line, 0, -(this.pos.distance(closest) - this.intensity));
+                }
+                graphics.lineGradientStyle(1, 0xffffffff, 0xffffffff, 0xFFFFFF00, 0xFFFFFF00, 1);
                 graphics.strokeLineShape(line);
             }
         }
